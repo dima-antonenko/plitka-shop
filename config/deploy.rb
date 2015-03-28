76a970f49f9e19c88be8edceab5c9ea8f4980638
dima-antonenko <d.antonenko94@mail.ru>
2015-03-28 19:49:07 +0300

    remove database file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repo_url, 'https://github.com/dima-antonenko/plitka-shop.git'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/laston'
+set :tmp_dir, "/home/deploy/tmp"
 
 # Default value for :scm is :git
 # set :scm, :git
